e69f570fbf50f507dda87938065799452f151161
unknown <joel.kobara@strathmore.edu>
2021-01-02 18:43:38 +0300

    Cart List created

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -48,6 +48,11 @@
     .search-box{
         width:500px !important;
     }
+    .cart-list-divider{
+        border-bottom: 1px solid #ccc;
+        margin-bottom: 20px;
+        padding-bottom: 20px;
+    }
     
 </style>
 
